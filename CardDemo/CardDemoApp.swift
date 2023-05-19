d3c15c59e183ee96a5a3db1d1e17def3de40e44b
Ike Mattice <ike.mattice@alaskaair.com>
2023-05-19 14:43:34 -0700

    Initial Commit

---
+++ CardDemo/CardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardDemoApp.swift
+//  CardDemo
+//
+//  Created by Ike Mattice on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
